6c3183096b1c51ded443ee3c51e7d144b4dc6d49
shen <santi.dionis@gmail.com>
2022-12-30 20:55:54 +0100

    ADD: add event listener token

--- main.js
+++ main.js
@@ -1,3 +1,94 @@
+const elts = {
+	text1: document.getElementById("text1"),
+	text2: document.getElementById("text2")
+};
+
+// The strings to morph between. You can change these to anything you want!
+const texts = [
+	"Why",
+	"is",
+	"this",
+	"so",
+	"satisfying",
+	"to",
+	"watch?"
+];
+
+// Controls the speed of morphing.
+const morphTime = 1;
+const cooldownTime = 0.25;
+
+let textIndex = texts.length - 1;
+let time = new Date();
+let morph = 0;
+let cooldown = cooldownTime;
+
+elts.text1.textContent = texts[textIndex % texts.length];
+elts.text2.textContent = texts[(textIndex + 1) % texts.length];
+
+function doMorph() {
+	morph -= cooldown;
+	cooldown = 0;
+	
+	let fraction = morph / morphTime;
+	
+	if (fraction > 1) {
+		cooldown = cooldownTime;
+		fraction = 1;
+	}
+	
+	setMorph(fraction);
+}
+
+// A lot of the magic happens here, this is what applies the blur filter to the text.
+function setMorph(fraction) {
+	// fraction = Math.cos(fraction * Math.PI) / -2 + .5;
+	
+	elts.text2.style.filter = `blur(${Math.min(8 / fraction - 8, 100)}px)`;
+	elts.text2.style.opacity = `${Math.pow(fraction, 0.4) * 100}%`;
+	
+	fraction = 1 - fraction;
+	elts.text1.style.filter = `blur(${Math.min(8 / fraction - 8, 100)}px)`;
+	elts.text1.style.opacity = `${Math.pow(fraction, 0.4) * 100}%`;
+	
+	elts.text1.textContent = texts[textIndex % texts.length];
+	elts.text2.textContent = texts[(textIndex + 1) % texts.length];
+}
+
+function doCooldown() {
+	morph = 0;
+	
+	elts.text2.style.filter = "";
+	elts.text2.style.opacity = "100%";
+	
+	elts.text1.style.filter = "";
+	elts.text1.style.opacity = "0%";
+}
+
+// Animation loop, which is called every frame.
+function animate() {
+	requestAnimationFrame(animate);
+	
+	let newTime = new Date();
+	let shouldIncrementIndex = cooldown > 0;
+	let dt = (newTime - time) / 5000;
+	time = newTime;
+	
+	cooldown -= dt;
+	
+	if (cooldown <= 0) {
+		if (shouldIncrementIndex) {
+			textIndex++;
+		}
+		
+		doMorph();
+	} else {
+		doCooldown();
+	}
+}
+
+// Start the animation.
+animate();
 let matriz = [
     ['1', '2', '3'],
     ['4', '5', '6'],
@@ -6,6 +97,14 @@ let matriz = [
 
 let board = document.querySelector('.board');
 //console.log(board);
+drawTokens()
+addEventListeners()
+
+
+
+
+
+
 
 function drawTokens(){
     matriz.forEach(row => row.forEach(element => {
@@ -16,9 +115,14 @@ function drawTokens(){
             board.innerHTML += `<div class ='token'>${element}</div>`
 
         }
-    }
-        ))
+    }))
 }
 
-drawTokens()
+function addEventListeners(){
+    let tokens = document.querySelectorAll('.token')
+    tokens.forEach(token => token.addEventListener('click', ()=> {console.log('se hizo click')}))
+}
+
+
+
 
