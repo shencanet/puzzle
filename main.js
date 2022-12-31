4c6655f1081e976b065078b28b04c309dae03600
shen <santi.dionis@gmail.com>
2022-12-31 16:33:22 +0100

    ADD: posiciones validas

--- main.js
+++ main.js
@@ -1,18 +1,10 @@
 const elts = {
-	text1: document.getElementById("text1"),
-	text2: document.getElementById("text2")
+  text1: document.getElementById("text1"),
+  text2: document.getElementById("text2"),
 };
 
 // The strings to morph between. You can change these to anything you want!
-const texts = [
-	"Why",
-	"is",
-	"this",
-	"so",
-	"satisfying",
-	"to",
-	"watch?"
-];
+const texts = ["Why", "is", "this", "so", "satisfying", "to", "watch?"];
 
 // Controls the speed of morphing.
 const morphTime = 1;
@@ -27,102 +19,138 @@ elts.text1.textContent = texts[textIndex % texts.length];
 elts.text2.textContent = texts[(textIndex + 1) % texts.length];
 
 function doMorph() {
-	morph -= cooldown;
-	cooldown = 0;
-	
-	let fraction = morph / morphTime;
-	
-	if (fraction > 1) {
-		cooldown = cooldownTime;
-		fraction = 1;
-	}
-	
-	setMorph(fraction);
+  morph -= cooldown;
+  cooldown = 0;
+
+  let fraction = morph / morphTime;
+
+  if (fraction > 1) {
+    cooldown = cooldownTime;
+    fraction = 1;
+  }
+
+  setMorph(fraction);
 }
 
 // A lot of the magic happens here, this is what applies the blur filter to the text.
 function setMorph(fraction) {
-	// fraction = Math.cos(fraction * Math.PI) / -2 + .5;
-	
-	elts.text2.style.filter = `blur(${Math.min(8 / fraction - 8, 100)}px)`;
-	elts.text2.style.opacity = `${Math.pow(fraction, 0.4) * 100}%`;
-	
-	fraction = 1 - fraction;
-	elts.text1.style.filter = `blur(${Math.min(8 / fraction - 8, 100)}px)`;
-	elts.text1.style.opacity = `${Math.pow(fraction, 0.4) * 100}%`;
-	
-	elts.text1.textContent = texts[textIndex % texts.length];
-	elts.text2.textContent = texts[(textIndex + 1) % texts.length];
+  // fraction = Math.cos(fraction * Math.PI) / -2 + .5;
+
+  elts.text2.style.filter = `blur(${Math.min(8 / fraction - 8, 100)}px)`;
+  elts.text2.style.opacity = `${Math.pow(fraction, 0.4) * 100}%`;
+
+  fraction = 1 - fraction;
+  elts.text1.style.filter = `blur(${Math.min(8 / fraction - 8, 100)}px)`;
+  elts.text1.style.opacity = `${Math.pow(fraction, 0.4) * 100}%`;
+
+  elts.text1.textContent = texts[textIndex % texts.length];
+  elts.text2.textContent = texts[(textIndex + 1) % texts.length];
 }
 
 function doCooldown() {
-	morph = 0;
-	
-	elts.text2.style.filter = "";
-	elts.text2.style.opacity = "100%";
-	
-	elts.text1.style.filter = "";
-	elts.text1.style.opacity = "0%";
+  morph = 0;
+
+  elts.text2.style.filter = "";
+  elts.text2.style.opacity = "100%";
+
+  elts.text1.style.filter = "";
+  elts.text1.style.opacity = "0%";
 }
 
 // Animation loop, which is called every frame.
 function animate() {
-	requestAnimationFrame(animate);
-	
-	let newTime = new Date();
-	let shouldIncrementIndex = cooldown > 0;
-	let dt = (newTime - time) / 5000;
-	time = newTime;
-	
-	cooldown -= dt;
-	
-	if (cooldown <= 0) {
-		if (shouldIncrementIndex) {
-			textIndex++;
-		}
-		
-		doMorph();
-	} else {
-		doCooldown();
-	}
+  requestAnimationFrame(animate);
+
+  let newTime = new Date();
+  let shouldIncrementIndex = cooldown > 0;
+  let dt = (newTime - time) / 5000;
+  time = newTime;
+
+  cooldown -= dt;
+
+  if (cooldown <= 0) {
+    if (shouldIncrementIndex) {
+      textIndex++;
+    }
+
+    doMorph();
+  } else {
+    doCooldown();
+  }
 }
 
 // Start the animation.
 animate();
+
+/* fila*columna
+[0.0],[0.1].[0.2]
+[1.0],[1.1].[1.2]
+[2.0],[2.1].[2.2]
+*/
+
 let matriz = [
-    ['1', '2', '3'],
-    ['4', '5', '6'],
-    ['7', '8', ''],
-]
+  ["1", "2", "3"],
+  ["4", "", "6"],
+  ["7", "8", "9"],
+];
 
-let board = document.querySelector('.board');
+let board = document.querySelector(".board");
 //console.log(board);
-drawTokens()
-addEventListeners()
+drawTokens();
+addEventListeners();
 
-
-
-
-
-
-
-function drawTokens(){
-    matriz.forEach(row => row.forEach(element => {
-        if(element == ''){
-            board.innerHTML += `<div class ='empty'>${element}</div>`
-      
-        }else{
-            board.innerHTML += `<div class ='token'>${element}</div>`
-
-        }
-    }))
+function drawTokens() {
+  matriz.forEach((row) =>
+    row.forEach((element) => {
+      if (element == "") {
+        board.innerHTML += `<div class ='empty'>${element}</div>`;
+      } else {
+        board.innerHTML += `<div class ='token'>${element}</div>`;
+      }
+    })
+  );
 }
 
-function addEventListeners(){
-    let tokens = document.querySelectorAll('.token')
-    tokens.forEach(token => token.addEventListener('click', ()=> {console.log('se hizo click')}))
+function addEventListeners() {
+  let tokens = document.querySelectorAll(".token");
+  tokens.forEach(token =>
+    token.addEventListener("click", ()=>{
+      let actualPosition = searchPosition(token.innerText)
+	  //console.log(actualPosition)
+	  let emptyPosition =searchPosition('')
+	  //console.log(emptyPosition)
+	  let movement = nextMovement(actualPosition, emptyPosition)
+    })
+  );
 }
 
+function searchPosition(element) {
+  let rowIndex = 0;
+  let columnIndex = 0;
+  matriz.forEach((row, index) => {
+    let rowElement = row.findIndex(item => item == element)
+    if (rowElement !== -1) {
+      //console.log(rowElement, index);
+	  rowIndex = index;
+	  columnIndex = rowElement
+     
+    }
+  })
+  return [rowIndex, columnIndex]
+}
 
-
-
+function nextMovement(actualPosition, emptyPosition){
+	if(actualPosition[0]-emptyPosition[0] ==-1){
+		console.log('abajo')
+	}else if(actualPosition[0]-emptyPosition[0] ==1){
+		console.log('arriba')
+	}else if(actualPosition[0] == emptyPosition[0]){
+		if(actualPosition[1]-emptyPosition[1] == -1){
+			console.log('Derecha');
+		}else if(actualPosition[1]-emptyPosition[1]== 1){
+			console.log('izquierda');
+		}
+	}else{
+		console.log('Sin movimiento')
+	}
+}
